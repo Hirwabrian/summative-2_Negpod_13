bee9bb908a9c74a41a9e98071b818bb0239880b0
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-02-24 22:33:04 +0200

    Update Select-emails.sh

--- Select-emails.sh
+++ Select-emails.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-sed -n 's/.*\b\([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]\{2,\}\)\b.*/\1/p' students-list_1023.txt | sort | awk '{print NR, $0}' > student-emails.txt
-
+sed 's/\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]\{2,\}\b/&/g' Students-list_1023.txt | sort | awk '{print NR, $0}' > student-emails.txt
 
