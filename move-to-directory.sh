501d06c57fec4ee0240a3216c765f4379d25a491
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-02-24 22:41:34 +0200

    Update move-to-directory.sh

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -2,7 +2,7 @@
 
 script_dir="$(cd "$(dirname "$0}")" && pwd)"
 mkdir -p $script_dir/negpod_13-q1
-files=("main.sh" "Students-list_1023.txt" "student-emails.txt" "Select-emails.sh")
+files=("Main.sh" "students-list_1023.txt" "student-emails.txt" "Select-emails.sh")
 for file in "${files[@]}";
 do
         mv "/summative-2_Negpod_13/$file" "$script_dir/negpod_13-q1"
